f331a4f59431466db3498e520df856c0646dbab9
Daniel Valenzuela <danvalen1@gmail.com>
2020-11-11 18:06:17 -0500

    Viz

--- src/modules/CoeffViz.py
+++ src/modules/CoeffViz.py
@@ -13,35 +13,25 @@ sns.set(font_scale = fontscale, style = 'whitegrid')
 markersize = 75
 
 
-labels_dict = {'PESEX_x': 'Sex',
-               'PREMPNOT_y': 'Employment Status',
-               'PRTAGE_x': 'Age',
-               'target': 'Employment Status',
-               'PEHRACTT': 'Hours Worked in a Week'
+labels_dict = {
+               'Features': 'Features',
+               'LogRegCoeff': 'Logistic Regression\n Coefficients'
                
               }
 
-ticks_dict = {'PESEX_x': ['Male', 'Female'],
-              'PREMPNOT_y': ['Employed', 'Unemployed', 'NILF\nDiscouraged', 'NILF\nOther'],
-              'target': ['Employed', 'Not Employed']
+ticks_dict = {'LogRegCoeff': [0, 1, 2, 4, 5, 6, 7, 8, 9],
              }
 
 
 
-def PlotCat(df, xvar, targetdir, yvar=None, kind='count', hue=None, palette="coolwarm"):
+def PlotCat(df, xvar,targetdir, yvar=None, orient=None, kind='count',palette="coolwarm"):
     """Plot a categorical plot with automatic labels provided in a global dict in CustomModule. 
         Pass a dataframe through `df`, a string through `xvar`, and where to save the image through
         `targetdir`. 
     """
     m_label = month_label(xvar)
     
-    if yvar:
-        y_label = labels_dict[yvar] + ' in April 2020'
-    else:
-        y_label = 'Frequency'
-        
-    
-    title = f'{y_label} vs. \n{labels_dict[xvar]} Categories{m_label}'
+    title = f'Frequency of {labels_dict[xvar]} Categories{m_label}'
     
 
     
@@ -50,30 +40,26 @@ def PlotCat(df, xvar, targetdir, yvar=None, kind='count', hue=None, palette="coo
                 x=xvar,
                 y=yvar,
                 kind = kind,
-                hue = hue,
+                orient = orient,
                 height = figsize[0],
                 aspect = figsize[1]/figsize[0],
                 palette=palette
                )
     plt.xlabel(labels_dict[xvar])
-    plt.ylabel(y_label)
+    plt.ylabel('Frequency')
     plt.title(title)
     labels = ticks_dict[xvar]
     plt.xticks(list(range(len(labels))), labels)
     if 'PREMPNOT' in xvar:
         plt.xticks(rotation=-25)
-    
-    
-    if 'PEHRACTT' in yvar:
-        plt.ylim((0, 140))
+        
     fig.savefig(f'{targetdir}{title}.png', bbox_inches='tight')
 
     return plt.show()
 
 def month_label(var):
-    if ('_y' in var) or ('target' in var):
+    if '_y' in var:
         var = ' in July 2020'
     else:
         var= ''
     return var
-        
